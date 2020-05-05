c909d0d50dcbb56ff7d119a3b897e728bfb4b696
github_username <you@domain.com>
2020-05-05 23:33:49 +0430

    first

---
+++ simple_social_clone/MyDjangoEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\simple_social_clone\mydjangoenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
